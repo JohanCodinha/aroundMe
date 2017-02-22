4bf06b9b299f8d528ed2a5e919d6f63cb4a9d079
Johan Codinha <johan@codinha.net>
2017-02-22 20:37:34 +1100

    species record list display properly

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,34 @@
     <h1 class="md-title">Around me</h1>
   </md-toolbar>
   <md-layout md-align="center" md-gutter="16">
-    <md-button @click.native="sayHi()" class="md-fab md-fab-bottom-right">
+    <md-button @click.native="getRecordsByLoc()" class="md-fab md-fab-bottom-right">
       <md-icon >my_location</md-icon>
     </md-button>
   </md-layout>
-  <md-list-item class="md-list-item">
-    <md-avatar>
-      <img src="https://placeimg.com/40/40/people/1" alt="People">
-    </md-avatar>
+  <md-list>
+    <md-list-item v-for="record in records" :record="record" class="md-list-item">
+      <md-avatar>
+        <img :src="`https://placeimg.com/40/40/animals/${record.taxonId}`" alt="People">
+      </md-avatar>
 
-    <div class="md-list-text-container">
-      <span>Common Bent-wing Bat</span>
-      <span>Miniopterus schreibersii GROUPi</span>
-      <p>Captured and released</p>
-    </div>
+      <div class="md-list-text-container">
+        <span>{{record.commonNme}}</span>
+        <span>{{record.scientificDisplayNme}}</span>
+        <p>{{record.siteLocationDesc}}</p>
+      </div>
 
-    <md-button class="md-icon-button md-list-action">
-      <md-icon class="md-primary">info</md-icon>
-    </md-button>
+      <md-button class="md-icon-button md-list-action">
+        <md-icon class="md-primary">info</md-icon>
+      </md-button>
 
-    <md-divider class="md-inset"></md-divider>
-  </md-list-item>
-  <md-list-item>
-    <md-divider class="md-inset"></md-divider>
-  </md-list-item>
-
+      <md-divider class="md-inset"></md-divider>
+    </md-list-item>
+  </md-list>
   <pre>{{$data}}</pre>
   </div>
 </template>
 
 <script>
-/* eslint-disable */
-/*global Vue */
-
-
 // import Hello from './components/Hello';
 
 export default {
@@ -51,28 +45,23 @@ export default {
     return data;
   },
   methods: {
-    sayHi: function() {
-      console.log('boooom');
-    },
-
-    getRecordsByLoc: () => {
-      // const vm = this;
-      debugger;
+    getRecordsByLoc() {
       const token = this.token;
-      console.log('wait what');
-      this.$http.get('http://localhost:3000/search/point', {
+      const params = {
         lat: -37.788151,
         long: 144.982671,
         rad: 250,
         token,
-      })
+      };
+      console.log(token);
+      this.$http.get('http://localhost:3000/search/point', { params })
         .then((res) => {
           console.log(res);
-          this.records = res;
+          this.records = res.body.species;
         });
     },
 
-    getToken: function() {
+    getToken() {
       this.$http
       .get('http://localhost:3000/auth/guest')
       .then((res) => {
