6407e042a70df2a3ff2925e594659965455f5d2a
Johan Codinha <johan@codinha.net>
2017-02-25 19:40:08 +1100

    pushing to heroku

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
   <md-toolbar>
     <h1 class="md-title">Around me</h1>
   </md-toolbar>
   <md-layout md-align="center" md-gutter="16">
-    <md-button @click.native="getRecordsByLoc()" class="md-fab md-fab-bottom-right">
-      <md-icon >my_location</md-icon>
-    </md-button>
   </md-layout>
   <md-list>
-    <md-list-item v-for="record in records" :record="record" class="md-list-item">
+    <md-list-item v-for="specie in species" :specie="specie" class="md-list-item">
       <md-avatar>
-        <img :src="`https://placeimg.com/40/40/animals/${record.taxonId}`" alt="People">
+        <img :src="`https://placeimg.com/40/40/animals/${specie.taxonId}`" alt="People">
       </md-avatar>
 
       <div class="md-list-text-container">
-        <span>{{record.commonNme}}</span>
-        <span>{{record.scientificDisplayNme}}</span>
-        <p>{{record.siteLocationDesc}}</p>
+        <span>{{specie.commonNme}}</span>
+        <span>{{specie.scientificDisplayNme}}</span>
+        <p>Total count : {{specie.totalCountInt}}</p>
       </div>
 
       <md-button class="md-icon-button md-list-action">
@@ -27,13 +24,15 @@
       <md-divider class="md-inset"></md-divider>
     </md-list-item>
   </md-list>
-  <pre>{{$data}}</pre>
+  <md-button @click.native="getRecordsByLoc()" style="position: fixed" class="md-button md-fab md-fab-bottom-right">
+    <md-icon >my_location</md-icon>
+  </md-button>
+  <!-- <pre>{{species}}</pre> -->
   </div>
 </template>
 
 <script>
 // import Hello from './components/Hello';
-
 export default {
   name: 'app',
   // props: ['records'],
@@ -44,29 +43,84 @@ export default {
     };
     return data;
   },
-  methods: {
-    getRecordsByLoc() {
-      const token = this.token;
-      const params = {
-        lat: -37.788151,
-        long: 144.982671,
-        rad: 250,
-        token,
-      };
-      console.log(token);
-      this.$http.get('http://localhost:3000/search/point', { params })
-        .then((res) => {
-          console.log(res);
-          this.records = res.body.species;
-        });
+  computed: {
+    species() {
+      return this.records.reduce((accuSpecies, specie) => {
+        // specie already present in species ? increment count : add to species
+        if (accuSpecies.some(accuSpecie => accuSpecie.taxonId === specie.taxonId)) {
+          accuSpecies.map((accuSpecie) => {
+            if (accuSpecie.taxonId !== specie.taxonId) return accuSpecie;
+            /* eslint-disable no-param-reassign */
+            accuSpecie.totalCountInt += specie.totalCountInt;
+            /* eslint-enable no-param-reassign */
+            return accuSpecie;
+          });
+        } else {
+          accuSpecies.push({
+            commonNme: specie.commonNme,
+            scientificDisplayNme: specie.scientificDisplayNme,
+            taxonId: specie.taxonId,
+            totalCountInt: specie.totalCountInt,
+          });
+        }
+        console.log(accuSpecies);
+        return accuSpecies;
+      }, []);
     },
+  },
 
+  methods: {
     getToken() {
       this.$http
       .get('http://localhost:3000/auth/guest')
       .then((res) => {
         this.token = res.body.jwt;
       });
+    },
+
+    getLocation() {
+      function error(e) {
+        console.log(e);
+      }
+
+      return new Promise((resolve, reject) => {
+        if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const accu = position.coords.accuracy;
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
+
+            console.log(`gps fixed at: ${position.coords} | ${position.coords.accuracy}`);
+
+            resolve({ accu, lat, long });
+          }, error,
+            {
+              enableHighAccuracy: true,
+              timeout: 10000,
+              maximumAge: 0 });
+        } else reject('no geolocation feature present on device');
+      });
+    },
+
+    getRecordsByLoc() {
+      const token = this.token;
+
+      this.getLocation()
+        .then((location) => {
+          const params = {
+            lat: location.lat,
+            long: location.long,
+            rad: 250,
+            token,
+          };
+
+          this.$http.get('http://localhost:3000/search/point', { params })
+            .then((res) => {
+              console.log(res);
+              this.records = res.body.species;
+              console.log(this.records);
+            });
+        });
     },
   },
 
@@ -79,4 +133,17 @@ export default {
 .md-list-item {
   list-style-type: none;
 }
+.app-viewport {
+  display: flex;
+  flex-flow: column;
+}
+
+
+html,
+body,
+.app {
+  height: 100%;
+  overflow: hidden;
+}
+
 </style>
