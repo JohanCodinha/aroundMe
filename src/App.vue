c7f163d26c996b13b73eaaf60693db3b2be7d295
Johan Codinha <johan@codinha.net>
2017-03-01 14:13:32 +1100

    updated vue-material dependencies, linked to fork

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-  <md-toolbar>
-    <h1 class="md-title">Around me</h1>
-  </md-toolbar>
-
-  <speciesList></speciesList>
-   <div>
-    <md-button class="md-raised md-accent" @click.native="toggleRightSidenav">Toggle right</md-button>
-    <p>Open console to see the events</p>
-  </div>
-  <md-sidenav class="md-right" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
     <md-toolbar>
-      <div class="md-toolbar-container">
-        <h3 class="md-title">Sidenav content</h3>
-      </div>
+      <h1 class="md-title">Around me</h1>
     </md-toolbar>
 
-    <md-button class="md-raised md-accent" @click.native="closeRightSidenav">Close</md-button>
-  </md-sidenav>
+    <speciesList></speciesList>
+     <div>
+      <md-button class="md-raised md-accent" @click.native="toggleRightSidenav">Toggle right</md-button>
+      <p>Open console to see the events</p>
+    </div>
+    <md-sidenav class="md-bottom" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
+      <md-toolbar>
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
 
+      <md-button class="md-raised md-accent" @click.native="closeRightSidenav">Close</md-button>
+    </md-sidenav>
+  </div>
 </template>
 
 <script>
